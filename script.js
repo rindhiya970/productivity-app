c0a80b22d7ebaef9f574bd4c463e27927bedcf5c
rindhiya970 <rindhiya731@gmail.com>
2025-09-04 23:08:13 +0530

    time42

--- script.js
+++ script.js
@@ -122,4 +122,6 @@
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     dateElement.textContent = today.toLocaleDateString('en-US', options);
   }
- // Call this on load
+ // Call this 
+ // 
+  updateDate();on load
