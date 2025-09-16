cd08888a1ca96374b88e55411b3a8b0a638b7ae7
rindhiya970 <rindhiya731@gmail.com>
2025-09-16 23:17:10 +0530

    time74

--- script.js
+++ script.js
@@ -118,7 +118,7 @@
 
     window.onload = loadTasks;  
 
-  function updateDate() {
+  function updateDate() {     
     const dateElement = document.getElementById('currentDate');
     const today = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
