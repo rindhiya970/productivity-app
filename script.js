3edb87d9485f45f8f663a851f69ef06406d68908
rindhiya970 <rindhiya731@gmail.com>
2025-09-17 23:44:06 +0530

    time78

--- script.js
+++ script.js
@@ -120,7 +120,7 @@
 
   function updateDate() {     
     const dateElement = document.getElementById('currentDate');
-    const today = new Date();
+    const today = new Date(); 
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     dateElement.textContent = today.toLocaleDateString('en-US', options);
   }
