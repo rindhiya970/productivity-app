c3d1f4660f3e951b7297b2374bd7eae0e92d715c
rindhiya970 <rindhiya731@gmail.com>
2025-09-20 15:10:12 +0530

    time96

--- script.js
+++ script.js
@@ -96,7 +96,7 @@
       document.getElementById('progressBar').style.width = percentage + '%';
       document.getElementById('progressText').textContent = `${completedTasks} of ${totalTasks} tasks completed (${percentage}%)`;
 
-      if (percentage === 100 && totalTasks !== 0) {
+      if (percentage === 100 && totalTasks !== 0) {   
         celebrate();
       }
     }
